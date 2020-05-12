83c274dd4334b14a19e4791c47ab6df0ef38a3c8
Mekarosi <mekarosi_assa@yahoo.com>
2020-05-12 22:41:03 +0100

    Setup MongoDB Database

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const connectDB = require('./config/db');
 const PORT = process.env.PORT || 4080;
+
+// Connect Database
+connectDB();
 
 app.use(express.json({ extended: false }));
 

d935a3e847307a51f87c97da90233e65fa1b2ee2
Mekarosi <mekarosi_assa@yahoo.com>
2020-05-15 06:57:33 +0100

    Create components

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import CreateTodo from './components/CreateTodo';
+import DeleteTodo from './components/DeleteTodo';
+import EditTodo from './components/EditTodo';
+import TodoList from './components/TodoList';
 
 class App extends Component {
   render() {
     return (
-      <div className='container'>
-        <h1>Pet Manager Todo</h1>
-      </div>
+      <Router>
+        <div className='container'>
+          <h1>Pet Manager Todo</h1>
+          <br />
+          <Route path='/' exact component={TodoList} />
+          <Route path='/edit/:id' component={EditTodo} />
+          <Route path='/create' component={CreateTodo} />
+          <Route path='/delete/:id' component={DeleteTodo} />
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/DeleteTodo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DeleteTodo extends Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome to Delete Todo Component!!</p>
+      </div>
+    );
+  }
+}
+
+export default DeleteTodo;
